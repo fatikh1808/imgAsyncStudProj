f357cc8e2d90467e9653f3a3fff961f68fab1308
Fatikh <fatikh1808@MacBook-Air-Fatikh.local>
2020-12-23 11:46:45 +0300

    add asynchron load images to web worker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,45 @@
 import MainPage from "./pages/mainPages";
+import React, { useEffect } from 'react';
 
 function App() {
+
+    function registerSw() {
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', function() {
+            navigator.serviceWorker.register('sw.js').then(function(registration) {
+            // Успешная регистрация
+            console.log('ServiceWorker registration successful');
+            }, function(err) {
+            // При регистрации произошла ошибка
+            console.log('ServiceWorker registration failed: ', err);
+            });
+        });
+    }
+}
+
+function notifyMe() {
+return new Promise(function(resolve, reject) {
+    const permissionResult = Notification.requestPermission(function(result) {
+      // Поддержка устаревшей версии с функцией обратного вызова. Многие браузеры пока поддерживают эту версию
+        resolve(result);
+        });
+
+        if (permissionResult) {
+        permissionResult.then(resolve, reject);
+        }
+    })
+    .then(function(permissionResult) {
+        if (permissionResult !== 'granted') {
+        throw new Error('Permission not granted.');
+        }
+    });
+}
+
+    // useEffect(() => {
+    //     registerSw()
+    //     notifyMe()
+    // }, [])
+    
     return <MainPage />;
 }
 
--- src/worker.js
+++ src/worker.js
@@ -3,48 +3,125 @@ export default () => {
   // eslint-disable-next-line no-restricted-globals
 self.addEventListener("message", async e => {
 
-    let images = []
-
-        function* gen(arr) {
-            for (let i = 0; i < arr.length; i++) {
-                yield arr[i];
-            }
-        }
-
-        const genratorIter = gen(e.data);
-
-        async function hh() {
-            let n = genratorIter.next();
-            let imageURL;
-            let blob;
-            if (!n.done) {
-                if (n.value.uri === undefined) {
-                    imageURL = await URL.createObjectURL(n.value);
-                    // First, we'll fetch the image file
-                    const response = await fetch(imageURL)
-                    // Once the file has been fetched, we'll convert it to a `Blob`
-                    blob = await response.blob()
-                } else {
-                    let response = await fetch(n.value.uri);
-                    if (!response.ok) {
-                        throw new Error(response.status);
-                    } else {
-                        blob = await response.blob();
-                        imageURL = await URL.createObjectURL(blob);
-                    }
-                }    
-                images.push({
-                    imageURL: imageURL,
-                    blob: blob,
-                })
-                hh()
-            
+    let images = [];
+    let Blob;
+    let url
+    const promises = e.data.map(async el => {
+                if (el.uri === undefined) {
+                url = URL.createObjectURL(el)
+                const response = await fetch(url)
+                const fileBlob = await response.blob();
+                Blob = fileBlob;
             } else {
-            postMessage(images);
+                const response = await fetch(el.uri)
+                const fileBlob = await response.blob()
+                url = URL.createObjectURL(fileBlob)
+                Blob = fileBlob
             }
-        }
+            return images.push({
+                imageURL: url,
+                blob: Blob,
+            })
+
+    });
+    await Promise.all(promises)
+        postMessage(images)
+        
     
-        hh()
+
+    // e.data.forEach(async el => {
+    //     console.log("el", el)
+    //     let Blob;
+    //     let url
+    //     if (el.uri === undefined) {
+    //         url = URL.createObjectURL(el)
+    //         const t = await fetch(url)
+    //         const b = await t.blob();
+    //         Blob = b;
+    //     } else {
+    //         const t = await fetch(el.uri)
+    //         const b = await t.blob()
+    //         url = URL.createObjectURL(b)
+    //         Blob = b
+    //     }
+    //     images.push({
+    //         imageURL: url,
+    //         blob: Blob,
+    //     })
+    // });
+
+    // postMessage(images)
+
+
+
+
+    // let imageURL;
+    // let blob;
+    // for (let p in e.data) {
+    //     let item = e.data[p]
+    //     if (item.uri === undefined) {
+    //         console.log(e.data[p])
+    //         imageURL = await URL.createObjectURL(item);
+    //         // First, we'll fetch the image file
+    //         const response = await fetch(imageURL)
+    //         // Once the file has been fetched, we'll convert it to a `Blob`
+    //         blob = await response.blob()
+    //     } else {
+    //         let response = await fetch(item.uri);
+    //         if (!response.ok) {
+    //             throw new Error(response.status);
+    //         } else {
+    //             blob = await response.blob();
+    //             imageURL = await URL.createObjectURL(blob);
+    //         }
+    //     }
+    //     images.push({
+    //         imageURL: imageURL,
+    //         blob: blob,
+    //     })
+    // }
+    // postMessage(images)
+
+
+
+
+
+
+    //     function* gen(arr) {
+    //         for (let i = 0; i < arr.length; i++) {
+    //             yield arr[i];
+    //         }
+    //     }
+    //     const genratorIter = gen(e.data);
+    //     async function hh() {
+    //         let n = genratorIter.next();
+    //         let imageURL;
+    //         let blob;
+    //         if (!n.done) {
+    //             if (n.value.uri === undefined) {
+    //                 imageURL = await URL.createObjectURL(n.value);
+    //                 // First, we'll fetch the image file
+    //                 const response = await fetch(imageURL)
+    //                 // Once the file has been fetched, we'll convert it to a `Blob`
+    //                 blob = await response.blob()
+    //             } else {
+    //                 let response = await fetch(n.value.uri);
+    //                 if (!response.ok) {
+    //                     throw new Error(response.status);
+    //                 } else {
+    //                     blob = await response.blob();
+    //                     imageURL = await URL.createObjectURL(blob);
+    //                 }
+    //             }    
+    //             images.push({
+    //                 imageURL: imageURL,
+    //                 blob: blob,
+    //             })
+    //             hh()   
+    //         } else {
+    //         postMessage(images);
+    //         }
+    //     }
+    //     hh()
     });
 }
-
