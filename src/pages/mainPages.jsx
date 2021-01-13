09ee2c9bc878909ba4390a909861068d72595fab
Fatikh <fatikh1808@MacBook-Air-Fatikh.local>
2021-01-13 11:37:30 +0300

    adding the documentation to rightBar Components

--- src/pages/mainPages.jsx
+++ src/pages/mainPages.jsx
@@ -8,19 +8,18 @@ import "./mainPages.scss";
 
 const MainPage = () => {
     const [loaders, setLoaders] = useState([])
-    const [deleteTime, setDeleteTime] = useState({})
+    const [deleteItem, setDeleteItem] = useState({})
 
     return (
         <main className={"Main__Page"}>
             <LeftBar />
             <MainContent
                 loaders={loaders}
-                setDelete={setDeleteTime}
+                setDelete={setDeleteItem}
             />
             <Rightbar
                 setLoaders={setLoaders}
-                deleteTime={deleteTime}
-                setDeleteTime={setDeleteTime}
+                deleteItem={deleteItem}
             />
         </main>
     );
